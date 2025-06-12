2fff77aeaa469ec5f63a0b4e2512ddb345e8ee67
Chandu <pravinchandvisam@gmail.com>
2025-06-13 00:02:00 +0530

    Add jQuery to cached assets

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@ const ASSETS = [
   './modules/about.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js',
   'https://cdn.jsdelivr.net/npm/pdf-lib@1.17.1/dist/pdf-lib.min.js',
   'https://cdn.jsdelivr.net/npm/datatables.net@1.13.5/js/jquery.dataTables.min.js',
   'https://cdn.jsdelivr.net/npm/datatables.net-dt@1.13.5/css/jquery.dataTables.min.css'
